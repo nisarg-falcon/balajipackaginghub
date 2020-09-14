4b5c183c2f081468b507538851c847e87e304886
Nisarg Saple <nisargsaple@MacBook-Pro.local>
2020-09-14 15:29:58 +0530

    balaji

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var w = window.innerWidth;
+    if(w <= 991){
+        $('#productNav').removeClass('show');
+        $('#productNav2').removeClass('show');
+    }
+});
